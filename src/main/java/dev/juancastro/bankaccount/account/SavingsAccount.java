9cde0a3933cab6f2b4145229c3c8161900259775
juancastro000 <cdyutub@gmail.com>
2025-03-29 19:27:04 +0100

    test green saving account active/inactive

--- src/main/java/dev/juancastro/bankaccount/account/SavingsAccount.java
+++ src/main/java/dev/juancastro/bankaccount/account/SavingsAccount.java
@@ -3,12 +3,15 @@ package dev.juancastro.bankaccount.account;
 import java.util.Locale;
 
 public class SavingsAccount extends Account {
+    private boolean active;
+
     public SavingsAccount(float balance, float annualRate) {
         super(balance, annualRate);
+        active = balance >= 10000;
     }
 
     @Override
     public String print() {
-        return String.format(Locale.US, "Balance: %.1f, Annual Rate: %.1f%% Deposits: %d, Withdrawals: %d", balance, annualRate * 100, deposits, withdrawals);
+        return String.format(Locale.US, "Balance: %.1f, Annual Rate: %.1f%% Active: %b, Deposits: %d, Withdrawals: %d", balance, annualRate * 100, active, deposits, withdrawals);
     }
 }
