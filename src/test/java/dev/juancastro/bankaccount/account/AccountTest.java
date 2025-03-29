dc319f9734fadc81325483ca94773e4a779270d0
juancastro000 <cdyutub@gmail.com>
2025-03-29 18:53:36 +0100

    test red deposit

--- src/test/java/dev/juancastro/bankaccount/account/AccountTest.java
+++ src/test/java/dev/juancastro/bankaccount/account/AccountTest.java
@@ -6,12 +6,22 @@ import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
-    
+
     @Test
     void createAccount() {
-        Account account = new SavingsAccount(10000, 0.12f); 
-        
+        Account account = new SavingsAccount(10000, 0.12f);
+
         assertThat(account.print(), containsString("Balance: 10000.0"));
         assertThat(account.print(), containsString("Annual Rate: 12.0%"));
+    }
+
+    @Test
+    void deposit_shouldIncreaseBalanceAndDeposits() {
+        Account account = new SavingsAccount(5000, 0.12f);
+
+        account.deposit(3000);
+
+        assertThat(account.print(), containsString("Balance: 8000.0"));
+        assertThat(account.print(), containsString("Deposits: 1"));
     }
 }
