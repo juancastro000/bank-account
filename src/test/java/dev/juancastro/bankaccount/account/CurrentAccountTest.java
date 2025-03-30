f7ac386b60179f9c300203cdaa0bf79392026e4a
juancastro000 <cdyutub@gmail.com>
2025-03-30 21:13:09 +0200

    test red overdraft current account

--- src/test/java/dev/juancastro/bankaccount/account/CurrentAccountTest.java
+++ src/test/java/dev/juancastro/bankaccount/account/CurrentAccountTest.java
@@ -12,4 +12,12 @@ public class CurrentAccountTest {
 
         assertThat(account.print(), containsString("Overdraft: 1000.0"));
     }
+    @Test
+    public void testWithdrawWithOverdraft() {
+        CurrentAccount account = new CurrentAccount(3000, 12);
+        account.withdraw(4000);
+        String info = account.print();
+        assertThat(info, containsString("Balance: 0.0"));
+        assertThat("overdraft should be 1000", account.getOverdraft(), is(closeTo(1000.0f, 0.001f)));
+    }
 }
